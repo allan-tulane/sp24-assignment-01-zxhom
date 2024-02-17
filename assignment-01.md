f0544fdd8bc671ce669b3293acd64b2c6d089d02
zxhom <123410472+zxhom@users.noreply.github.com>
2024-02-17 22:41:51 +0000

    done

--- assignment-01.md
+++ assignment-01.md
@@ -2,7 +2,7 @@
 
 # CMPS 2200 Assignment 1
 
-**Name:**_________________________
+**Name:** Zachary Hom
 
 
 In this assignment, you will learn more about asymptotic notation, parallelism, functional languages, and algorithmic cost models. As in the recitation, some of your answer will go here and some will go in `main.py`. You are welcome to edit this `assignment-01.md` file directly, or print and fill in by hand. If you do the latter, please scan to a file `assignment-01.pdf` and push to your github repository. 
@@ -12,35 +12,38 @@ In this assignment, you will learn more about asymptotic notation, parallelism, 
 1. (2 pts ea) **Asymptotic notation** (12 pts)
 
   - 1a. Is $2^{n+1} \in O(2^n)$? Why or why not? 
+.  Yes, because there exists some c > 2 such that 2^{n+1} < c*2^n.
+lim as x --> infinity of [2^{n+1}/2^n] == 2.
 .  
-.  
-.  
+.
 .  
 . 
   - 1b. Is $2^{2^n} \in O(2^n)$? Why or why not?     
-.  
+.  No, because there does not exist some c such that 2^{2^n} < c*2^n.
+lim as x --> infinity of [2^{2^n}/2^n] --> infinity.
 .  
 .  
 .  
 .  
   - 1c. Is $n^{1.01} \in O(\mathrm{log}^2 n)$?    
+. No, because there does not exist some c such that n^{1.01} < c*\mathrm{log}^2 n for large values of n.
+lim as x --> infinity of [n^{1.01}/mathrm{log}^2 n] --> infinity.
 .  
 .  
 .  
-.  
-
   - 1d. Is $n^{1.01} \in \Omega(\mathrm{log}^2 n)$?  
-.  
+.  Yes, because lim as n --> infinity of [n^{1.01}/mathrm{log}^2 n] --> infinity which means that mathrm{log}^2 n bounds n^{1.01} from below.
 .  
 .  
 .  
   - 1e. Is $\sqrt{n} \in O((\mathrm{log} n)^3)$?  
+.  No, because there does not exist some value c such that sqrt{n} < c*mathrm{log} n)^3) for large values of n.
+lim as x --> infinity of [sqrt{n}/mathrm{log} n)^3] --> infinity.
 .  
 .  
 .  
-.  
-  - 1f. Is $\sqrt{n} \in \Omega((\mathrm{log} n)^3)$?  
-.  
+  - 1f. Is $\sqrt{n} \in \Omega((\mathrm{log} n)^3)$?
+.  Yes, because lim as x --> infinity of [sqrt{n}/mathrm{log} n)^3] --> infinity which means mathrm{log} n)^3 bounds sqrt{n} from below
 
 
 2. **SPARC to Python** (12 pts)
@@ -59,9 +62,10 @@ $$
 $$ 
 
   - 2a. (6 pts) Translate this to Python code -- fill in the `def foo` method in `main.py`  
+  DONE.
 
   - 2b. (6 pts) What does this function do, in your own words?  
-
+This function takes in an input and recursively calls itself with inputs x-1 and x-2 added together; therefore, it returns the x'th number in the Fibonacci sequence of numbers. 
 .  
 .  
 .  
@@ -88,10 +92,12 @@ def longest_run(myarray, key)
 ```
 E.g., `longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3`  
  
-  - 3a. (7 pts) First, implement an iterative, sequential version of `longest_run` in `main.py`.  
+  - 3a. (7 pts) First, implement an iterative, sequential version of `longest_run` in `main.py`.
+  DONE.
 
   - 3b. (4 pts) What is the Work and Span of this implementation?  
-
+Work: O(n) -- must run through each element of the input one at a time so runtime is O(n)
+Span: O(n) -- sequential, cannot run multiple iterations at the same time even with more processors
 .  
 .  
 .  
@@ -106,6 +112,9 @@ E.g., `longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3`
   - 3c. (7 pts) Next, implement a `longest_run_recursive`, a recursive, divide and conquer implementation. This is analogous to our implementation of `sum_list_recursive`. To do so, you will need to think about how to combine partial solutions from each recursive call. Make use of the provided class `Result`.   
 
   - 3d. (4 pts) What is the Work and Span of this sequential algorithm?  
+  ** Dr. Ding said this means the recursive function on ONE processor (no parallelization possible) **
+Work: O(n)
+Span: O(n) -- because parallelization is not possible in this scenario, span would still be O(n) because multiple threads created by recursion could not be run at the same time
 .  
 .  
 .  
@@ -120,7 +129,8 @@ E.g., `longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3`
 
 
   - 3e. (4 pts) Assume that we parallelize in a similar way we did with `sum_list_recursive`. That is, each recursive call spawns a new thread. What is the Work and Span of this algorithm?  
-
+Work: O(n)
+Span: O(log2_n) -- because parallelization is possible in this scenario, span would be O(log2_n) because multiple threads created by recursion can be run at the same time
 .  
 .  
 .  
--- main.py
+++ main.py
@@ -4,41 +4,148 @@ See assignment-01.pdf for details.
 """
 # no imports needed.
 
+
 def foo(x):
-    ### TODO
-    pass
+  if x <= 1:
+    return x
+
+  else:
+    ra = foo(x - 1)
+    rb = foo(x - 2)
+    return ra + rb
+
+
+def test_foo():
+  assert foo(0) == 0
+  assert foo(1) == 1
+  assert foo(2) == 1
+  assert foo(3) == 2
+  assert foo(4) == 3
+  assert foo(5) == 5
+  assert foo(6) == 8
+  assert foo(7) == 13
+
+
+test_foo()
+
 
 def longest_run(mylist, key):
-    ### TODO
-    pass
+  count = 0
+  max_count = 0
+  for i in range(len(mylist)):
+    if mylist[i] == key:
+      count += 1
+      #print("key: ", count)
+    else:
+      if count > max_count:
+        max_count = count
+        #print("if: ", count)
+        count = 0
+      else:
+        #print("else: ", count)
+        count = 0
+  if count > max_count:
+    max_count = count
+
+  #return print("max run: ", max_count)
+  return max_count
+
+
+def test_longest_run():
+  assert longest_run([2, 12, 12, 8, 12, 12, 12, 0, 12, 1], 12) == 3
+  assert longest_run([3, 2, 2, 3, 2, 2, 2, 2], 2) == 4
+  assert longest_run([3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1], 1) == 8
+  assert longest_run([1, 4, 5, 2, 9, 9, 9, 2, 6, 3, 9, 9, 1, 3, 9, 9], 9) == 3
+
+
+test_longest_run()
 
 
 class Result:
-    """ done """
-    def __init__(self, left_size, right_size, longest_size, is_entire_range):
-        self.left_size = left_size               # run on left side of input
-        self.right_size = right_size             # run on right side of input
-        self.longest_size = longest_size         # longest run in input
-        self.is_entire_range = is_entire_range   # True if the entire input matches the key
-        
-    def __repr__(self):
-        return('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
-              (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
-    
+  """ done """
+
+  def __init__(self, left_size, right_size, longest_size, is_entire_range):
+    self.left_size = left_size  # run on left side of input
+    self.right_size = right_size  # run on right side of input
+    self.longest_size = longest_size  # longest run in input
+    self.is_entire_range = is_entire_range  # True if the entire input matches the key
+
+  def __repr__(self):
+    return ('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
+            (self.longest_size, self.left_size, self.right_size,
+             self.is_entire_range))
+
 
 def to_value(v):
-    """
+  """
     if it is a Result object, return longest_size.
     else return v
     """
-    if type(v) == Result:
-        return v.longest_size
-    else:
-        return int(v)
-        
+  if type(v) == Result:
+    return v.longest_size
+  else:
+    return int(v)
+
+
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
+  # Base Case when mylist = 1
+  if len(mylist) == 1:
+    if mylist[0] == key:
+      return Result(1, 1, 1, True)
+
+    else:
+      return Result(0, 0, 0, False)
+  else:
+    # Recursive Case
+    mid = len(mylist) // 2
+    left = longest_run_recursive(mylist[:mid], key)
+    right = longest_run_recursive(mylist[mid:], key)
+    # Case 1: both sides are entirely the key
+    if (left.is_entire_range and right.is_entire_range) == True:
+      return Result(left.longest_size + right.longest_size,
+                    left.longest_size + right.longest_size,
+                    left.longest_size + right.longest_size, True)
+    # Case 2: left side is entirely the key but right is not
+    elif left.is_entire_range == True and right.is_entire_range == False:
+      return Result(
+          left.longest_size + right.left_size, right.right_size,
+          max(left.longest_size + right.left_size, right.longest_size), False)
+    # Case 3: right side is entirely the key but left is not
+    elif left.is_entire_range == False and right.is_entire_range == True:
+      # Run on left side to find longest run for middle case
+      return Result(
+          left.left_size, left.right_size + right.longest_size,
+          max(left.right_size + right.longest_size, left.longest_size), False)
+    else:
+      # checks for middle case when key runs in both left/right
+      return Result(
+          left.left_size, right.right_size,
+          max(left.longest_size, right.longest_size,
+              left.right_size + right.left_size), False)
 
 
+#print(to_value(longest_run_recursive([3, 2, 2, 3, 2, 2, 2, 2], 2)))
+#print(to_value(
+#           longest_run_recursive([3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1], 1)))
 
+
+def test_longest_run_recursive():
+  assert (to_value(
+      longest_run_recursive([2, 12, 12, 8, 12, 12, 12, 0, 12, 1],
+                            12)) == 3) == True
+  assert (to_value(longest_run_recursive([3, 2, 2, 3, 2, 2, 2, 2],
+                                         2)) == 4) == True
+  assert (to_value(
+      longest_run_recursive([3, 3, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1],
+                            1)) == 8) == True
+  assert (to_value(
+      longest_run_recursive([1, 4, 5, 2, 9, 9, 9, 2, 6, 3, 9, 9, 1, 3, 9, 9],
+                            9)) == 3) == True
+  assert (to_value(
+      longest_run_recursive([1, 4, 5, 2, 9, 9, 9, 2, 6, 3, 9, 9, 1, 3, 9, 9],
+                            10)) == 0) == True
+  assert (to_value(longest_run_recursive([2, 2, 2, 2, 2, 2, 2, 2, 2],
+                                         2)) == 9) == True
+
+
+test_longest_run_recursive()
